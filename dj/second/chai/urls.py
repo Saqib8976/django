e5e5e43615afbcf2e54324d3282e59344f9c6516
khan saqib <149991508+Saqib8976@users.noreply.github.com>
2025-08-13 21:07:49 +0530

    Update urls.py

--- dj/second/chai/urls.py
+++ dj/second/chai/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path("",form1,name="form1"),
+    path("form/",form1,name="form1"),
     path("addition/",add,name="add"),
 
 ]
